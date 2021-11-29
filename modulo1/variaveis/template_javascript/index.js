403c9e76d9836903c7ed9f0397e9f7036a0b2be5
CaiofrancaRj <caiostockmarket@outlook.com>
2021-11-29 16:27:32 -0300

    exercicios com variaveis js

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,111 @@
+//exercício 1
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//Análise:        Na entrada de dados, o programa pede para que se assuma valores mutáveis para as varáveis a e b. 
+//Ele imprime o valor da variável b (10)
+//Ele muda o valor da variável b, para 5, e imprime então, os valores correspondentes às duas variáveis.
+//10 e 5 ( a variável b, é mostrada com seu novo valor.)
+
+
+
+
+
+//exercício 2
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+//Análise: Na entrada de dados, o programa pede para assumir valores mutáveis às duas variáveis (a,b)
+//Logo em seguida, ele faz com que, consecutivamente, c assuma o valor 10, b passe a valer 10 por consequência e 
+//a, mantenha seu valor inicial.
+//Após isso, ele imprime três vezes o valor 10 
+
+
+
+
+
+
+//exercício 3
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+//Análise: Na entrada de dados, o programa faz uma pergunta ao usuário e assume um valor mutável para p com base na resposta 
+//em seguida, ele faz semelhante com a variável t 
+//Com posse dos dois dados inseridos pelo usuário, ele irá realizar uma conta matemática simples, e descobrir na saída de dados,
+// Junto à um alerta, o valor exato de quanto o usuário recebe por hora trabalhada proporcionalmente. 
+
+
+//exercício escrita 1
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+
+// console.log( typeof nome )
+// console.log( typeof idade )
+
+// //O programa diz que ambas as variáveis são do tipo string, pois sem definir um tipo específico, string é o padrão.
+
+// let nomenovo = prompt("Qual o seu nome?")
+// let idadenovo = prompt("Qual a sua idade?")
+
+// console.log( nome = nomenovo )
+// console.log( idade = idadenovo )
+
+// console.log( typeof nomenovo )
+// console.log( typeof idadenovo )
+// //Elas permaneceram string, como era esperado neste caso.
+
+// console.log( "Olá,",nome, "você tem" ,idade, "anos." )
+
+
+
+
+
+//exercício de escrita 2 
+// const medicação = prompt("Você tomou a sua medicação hoje? Sim/Não")
+// const exercícios = prompt("Você realizou as suas atividades físicas? Sim/Não")
+// const alimentação = prompt("Você se alimentou corretamente hoje? Sim/Não")
+
+// console.log("Você toou sua medicação hoje?",medicação,)
+// console.log("Você realizou as suas atividades físicas?,",exercícios,)
+// console.log("Você se alimentou corretamente hoje?",alimentação,)
+
+
+//exercício de escrita 3 
+// let a = 50
+// let b = 70
+
+// c = a
+// a = b
+// b = c
+
+// console.log("O novo valor de a é", a) 
+// console.log("O novo valor de b é", b) 
+
+
+
+
+
+//Desafio
+
+
+// let num1 = Number(prompt("Insira um número:"))
+// let num2 = Number(prompt("Insira outro número:"))
+
+// let soma = num1+num2
+// let produto = num1*num2
+
+// console.log("O primeiro número somado ao segundo número resulta em:",soma,)
+// console.log("O primeiro número multiplicado pelo segundo número resulta em:",produto,)
+
+
