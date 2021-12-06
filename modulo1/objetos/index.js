f29aae75035918a47fa1004631d548591cabc4d9
CaiofrancaRj <caiostockmarket@outlook.com>
2021-12-06 16:51:12 -0300

    lista objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,88 @@
+// // Exercícios de interpretação
+// // 1
+
+// a :
+// Matheus Nachtergaele
+// Virginia Cavendish
+// { canal: 'Globo', horario: '14h' }
+
+// 2
+
+// a:
+// { nome: 'Juca', idade: 3, raca: 'SRD' }
+// { nome: 'Juba', idade: 3, raca: 'SRD' }
+// { nome: 'Jubo', idade: 3, raca: 'SRD' }
+
+// b:
+// Ela copia todas as informações do objeto indicado.
+
+// 3
+
+// a: 
+// false
+// undefined
+
+// b:
+// o nome backender estava definido com valor false, e o campo altura estava sem valor, portanto, indefinido. 
+
+// Exercícios de escrita:
+
+// 1
+// const pessoa = {
+//     nome: "Letícia",
+//     apelidos: ["Lê", "Lelê", "Lelezinha" ]
+       
+   
+// }
+
+// const apelido1 = pessoa.apelidos[0];
+// const apelido2 = pessoa.apelidos[1];
+// const apelido3 = pessoa.apelidos[2]
+
+// console.log(`Eu sou ${pessoa.nome} , mas pode me chamar de: ${apelido1} , ${apelido2} ou ${apelido3}. `)
+
+// // Saída do console : Eu sou Letícia , mas pode me chamar de: Lê , Lelê ou Lelezinha. 
+
+// const pessoa2 = {
+//     ...pessoa,
+//     apelidos: ["lelele", "lelelele", "Lêlêlê"]
+// }
+
+
+// console.log(`Eu sou ${pessoa2.nome} , mas pode me chamar de: ${pessoa2.apelidos[0]} , ${pessoa2.apelidos[1]} ou ${pessoa2.apelidos[2]}.`)
+
+// Eu sou Letícia , mas pode me chamar de: lelele , lelelele ou Lêlêlê.
+
+// 2:
+// const alguem = {
+// 	nome: "klaus", 
+//     idade: 56, 
+// 	profissao: "músico"
+
+// }
+// const resultado = [];
+// i = 0;
+// resultado.push(alguem.nome)
+// resultado.push(alguem.nome.length)
+// resultado.push(alguem.idade)
+// resultado.push(alguem.profissao)
+// console.log(resultado)
+
+// console>>>['klaus', 5, 56, 'músico']
+
+
+
+
+
+// var carrinho = []
+// const frutas = {
+//     nome1: "banana", disponível: true,
+//     nome2: "uva", disponível: false,
+//     nome3: "abacate" disponível: true  
+// }
+
+// function = frutas(banana, uva, abacate){
+//     return carrinho.push
+
+// }
+
